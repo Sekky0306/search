1ae05a05cbe7011e3cf7e6ddc85e5da798692edc
Sekky <step1336@gmail.com>
2015-05-08 21:50:41 +0900

    Initial Commit

---
+++ seach/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  seach
+//
+//  Created by 関戸優紀 on 2015/05/08.
+//  Copyright (c) 2015年 関戸優紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ seach/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  seach
+//
+//  Created by 関戸優紀 on 2015/05/08.
+//  Copyright (c) 2015年 関戸優紀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
